2a8b907e4e82470457adf76e48d2e8db4d454573
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-08-19 08:16:32 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+// Write your solution in this file
+customerName ='bob'
+
+upperCaseCustomerName = ()=>{
+ customerName="BOB"
+}
+setBestCustomer=()=>{
+    bestCustomer = "not bob"
+}
+overwriteBestCustomer=()=>{
+    bestCustomer="maybe bob"
+}
+changeLeastFavoriteCustomer=()=>{
+    const leastFavouriteCustomer="mike"
+     leastFavouriteCustomer="john"
+    
+
+}
